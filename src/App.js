f9d198837bc4b440c98fcf83eb050b49b9b00e80
talkiedoorkey <89324532+talkiedoorkey@users.noreply.github.com>
2022-02-05 16:13:38 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import axios from "axios";
+import "./styles.css";
+
+export default function App() {
+  const onClickUsers = () => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/users")
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((err) => console.log(err));
+  };
+  const onClickUser1 = () => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/users/3")
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((err) => console.log(err));
+  };
+
+  return (
+    <div className="App">
+      <button onClick={onClickUsers}>users</button>
+      <button onClick={onClickUser1}>idが1のuser</button>
+    </div>
+  );
+}
